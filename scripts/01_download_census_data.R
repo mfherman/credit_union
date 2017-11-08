c170d6535677ce5a8363727eaae16424723dcdcd
Matt Herman <mfherman@gmail.com>
2017-11-07 23:32:36 -0500

    init

---
+++ scripts/01_download_census_data.R
@@ -0,0 +1,76 @@
+library(tidyverse)
+library(tidycensus)
+library(sf)
+library(tmap)
+options(tigris_use_cache = TRUE)
+
+# total population B01001_001
+# % non hispanic white B03002_003
+# % non hispanic black B03002_004
+# % hispanic B03002_012
+# median age B01002_001
+# median hh income B19013_001y
+# below 100% poverty level B06012_002
+# poverty level denom B06012_001
+# gini index B19083_001
+# pub assistance/food stamp B19058_002
+# total households B19058_001
+# median gross rent as a pct of income B25071_001
+# lived in owner-occuppied B07013_002
+# live in renter-occuppied B07013_003
+# total housesholds for owner/rent B07013_001
+
+# select counties
+atlanta_msa <- c("Fulton", "DeKalb", "Gwinnett", "Cobb", "Clayton",
+                 "Coweta", "Douglas", "Fayette", "Henry")
+
+# select variables
+variables <- c("B01001_001", "B03002_003", "B03002_004", "B03002_012",
+               "B01002_001", "B19013_001", "B06012_002", "B06012_001",
+               "B19083_001", "B19058_002", "B19058_001", "B25071_001",
+               "B07013_002", "B07013_003", "B07013_001")
+               
+# download acs 5 year estimates
+atlanta_tract <- get_acs(
+  state = "GA",
+  county = atlanta_msa,
+  geography = "tract",
+  variables = variables,
+  survey = "acs5",
+  year = 2015,
+  geometry = TRUE,
+  output = "wide"
+  )
+
+# percentage and select variables, reproject to wgs84
+atl_tract_stat <- atlanta_tract %>%
+  mutate(
+    white_pct = B03002_003E / B01001_001E * 100,
+    black_pct = B03002_004E / B01001_001E * 100,
+    hisp_pct = B03002_012E / B01001_001E * 100,
+    pov_pct = B06012_002E / B06012_001E * 100,
+    pub_as_pct = B19058_002E / B19058_001E * 100,
+    own_pct = B07013_002E / B07013_001E * 100,
+    rent_pct = B07013_003E / B07013_001E * 100
+  ) %>%
+  select(
+    geoid = GEOID,
+    name = NAME,
+    pop_tot = B01001_001E,
+    med_age = B01002_001E,
+    med_hhinc = B19013_001E,
+    gini = B19083_001E,
+    med_rburd = B25071_001E,
+    white_pct:rent_pct
+  )
+
+# check out a simple map
+tm_shape(atl_tract_stat) +
+  tm_fill(
+    col = "black_pct",
+    n = 5,
+    style = "jenks"
+    )
+
+# write to geojson
+st_write(atl_tract_stat, "./output/atl_tract1.geojson", delete_dsn = TRUE)
