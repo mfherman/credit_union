4419bd7230096bd15a67798d4113f2315190fcbf
Matt Herman <mfherman@gmail.com>
2017-12-07 19:10:17 -0500

    update to 2016 number

--- scripts/01_download_census_data.R
+++ scripts/01_download_census_data.R
@@ -57,12 +57,13 @@ atlanta_msa <- c(
 
 # select variables
 tract_variables <- c("B01001_001", "B03002_003", "B03002_004", "B03002_006",
-                     "B03002_012","B01002_001", "B19013_001", "B06012_002",
-                     "B06012_001", "B19083_001", "B19058_002", "B19058_001",
+                     "B03002_012","B01002_001", "B19013_001", "B17021_002",
+                     "B17021_001", "B19083_001", "B19058_002", "B19058_001",
                      "B25071_001", "B07013_002", "B07013_003", "B07013_001")
 
 bg_variables <- c("B01001_001", "B03002_003", "B03002_004", "B03002_006",
-                  "B03002_012","B01002_001", "B19013_001", "B19058_002", "B25071_001")
+                  "B03002_012","B01002_001", "B19013_001", "B19058_002",
+                  "B25071_001", "B17021_002", "B17021_001")
                
 # download acs 5 year estimates tract
 atlanta_tract <- get_acs(
@@ -71,7 +72,7 @@ atlanta_tract <- get_acs(
   geography = "tract",
   variables = tract_variables,
   survey = "acs5",
-  year = 2015,
+  year = 2016,
   geometry = TRUE,
   output = "wide"
   )
@@ -83,7 +84,7 @@ atlanta_bg <- get_acs(
   geography = "block group",
   variables = bg_variables,
   survey = "acs5",
-  year = 2015,
+  year = 2016,
   geometry = TRUE,
   output = "wide"
 )
@@ -97,7 +98,7 @@ atl_tract_stat <- atlanta_tract %>%
     hisp_pct = B03002_012E / B01001_001E * 100,
     asian_pct = B03002_006E / B01001_001E * 100,
     other_pct = 100 - (white_pct + black_pct + hisp_pct + asian_pct),
-    pov_pct = B06012_002E / B06012_001E * 100,
+    pov_pct = B17021_002E / B17021_001E * 100,
     pub_as_pct = B19058_002E / B19058_001E * 100,
     own_pct = B07013_002E / B07013_001E * 100,
     rent_pct = B07013_003E / B07013_001E * 100
@@ -111,6 +112,8 @@ atl_tract_stat <- atlanta_tract %>%
     med_hhinc = B19013_001E,
     gini = B19083_001E,
     med_rburd = B25071_001E,
+    pop_pov = B17021_002E,
+    pop_pov_denom = B17021_001E,
     pop_white = B03002_003E,
     pop_black = B03002_004E,
     pop_hisp = B03002_012E,
@@ -123,12 +126,13 @@ atl_tract_stat <- atlanta_tract %>%
 # percentage and select variables, reproject to wgs84 
 atl_bg_stat <- atlanta_bg %>%
   mutate(
-    pop_other = B01001_001E - pmap_dbl(list(B03002_003E, B03002_004E, B03002_012E, B03002_006E), sum),
+    pop_other = B01001_001E - (B03002_003E + B03002_004E + B03002_012E + B03002_006E),
     white_pct = B03002_003E / B01001_001E * 100,
     black_pct = B03002_004E / B01001_001E * 100,
     hisp_pct = B03002_012E / B01001_001E * 100,
     asian_pct = B03002_006E / B01001_001E * 100,
-    other_pct = 100 - pmap_dbl(list(white_pct, black_pct, hisp_pct, asian_pct), sum)
+    other_pct = 100 - (white_pct + black_pct + hisp_pct + asian_pct),
+    pov_pct = B17021_002E / B17021_001E * 100
   ) %>%
   mutate_at(vars(ends_with("pct")), funs(round(., 1))) %>%
   select(
@@ -138,12 +142,14 @@ atl_bg_stat <- atlanta_bg %>%
     med_age = B01002_001E,
     med_hhinc = B19013_001E,
     med_rburd = B25071_001E,
+    pop_pov = B17021_002E,
+    pop_pov_denom = B17021_001E,
     pop_white = B03002_003E,
     pop_black = B03002_004E,
     pop_hisp = B03002_012E,
     pop_asian = B03002_006E,
     pop_other,
-    white_pct:other_pct
+    white_pct:pov_pct
   ) %>%
   st_transform(4326)
 
--- scripts/09_buffer.R
+++ scripts/09_buffer.R
@@ -3,6 +3,8 @@ library(sf)
 library(mapview)
 library(units)
 
+library(spatstat)
+
 atl_census_tract <- read_sf("./output/atl_tract.geojson") %>%
   st_transform(2240)
 
@@ -11,6 +13,9 @@ atl_bg <- read_sf("./output/atl_bg.geojson") %>%
   mutate(area_bg = st_area(.))
 
 atl_cu <- read_sf("./output/atl_msa_cu.geojson") %>%
+  st_transform(2240)
+
+atl_city_cu <- read_sf("./output/atl_cu.geojson") %>%
   st_transform(2240)
 
 atl_payday <- read_sf("./output/atl_financial.geojson") %>%
@@ -24,7 +29,6 @@ atl_fdic <- read_sf("./output/atl_fdic.geojson") %>%
 atl_msa <- read_sf("./output/atl_tract.geojson") %>%
   st_union()  %>%
   st_transform(2240)
-
 
 bg_buffer_calc <- function(x) {
   # create 1/4 mile buffers, clip to msa, calculate buffer area
@@ -58,13 +62,59 @@ bg_buffer_calc <- function(x) {
     summarize_at(vars(contains("med")), funs(weighted.mean(., pop_tot_adj, na.rm = TRUE))) %>%
     st_set_geometry(NULL)
   
-  cu_bg_summary <- left_join(pop_sum, med_sum, by = "id") %>%
-    summarise_at(vars(pop_black_adj_pct, pop_white_adj_pct, med_hhinc), funs(mean(., na.rm = TRUE)))
+  cu_bg_summary <- left_join(pop_sum, med_sum, by = "id")
   
   return(cu_bg_summary)
 }
+
+sum_bg <- function(x) {
+  summary <- summarise_at(x, vars(pop_black_adj_pct, pop_white_adj_pct, med_hhinc),
+                          funs(mean(., na.rm = TRUE)))
+  return(summary)
+}
+
+
+hi <- map(list("Credit Union" = atl_cu, "FDIC Bank" = atl_fdic, "Predatory Lenders" = atl_payday),
+    bg_buffer_calc)
+
+hi2 <- map_dfr(hi, sum_bg, .id = "Type")
+
+
+hi3 <- map_dfr(list("Credit Union" = atl_cu, "FDIC Bank" = atl_fdic, "Predatory Lender" = atl_payday),
+          bg_buffer_calc, .id = "Type")
+
+
+library(RColorBrewer)  
+library(scales)
+
+hi4 <- 
+
+hi3 %>%
+  ggplot() +
+  geom_density(aes(x = med_hhinc, y = ..density.. * 1e4, fill = Type), alpha = 0.6) +
+  theme_bw() +
+  scale_fill_brewer(palette = "Set2") +
+  scale_x_continuous(labels = dollar, limits = c(0, 1.75e5)) +
+  labs(
+    x = "Median Household Income",
+    y = "Density",
+    title = "Income Distribution of Neighborhoods Near Financial Institutions",
+    subtitle = "Atlanta-Sandy Springs-Roswell, GA Metro Area",
+    fill = "Type of Financial\nInstitution"
+  ) +
+  theme(
+    axis.title.x = element_text(size = 10, margin = margin(t = 10)),
+    axis.title.y = element_text(size = 10)
+  )
 
 summary_stats <- map_dfr(
   list("Credit Union" = atl_cu, "FDIC Bank" = atl_fdic, "Payday" = atl_payday),
   bg_buffer_calc, .id = "Type"
   )
+
+
+summarise_at(vars(pop_black_adj_pct, pop_white_adj_pct, med_hhinc), funs(mean(., na.rm = TRUE)))
+
+## POOR BLACK vs. POOR WHITE??
+
+
